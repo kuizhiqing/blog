f73e28ecff9ceffe9ca14407ee8d8b2074910a6e
kuizhiqing <kuizhiqing@users.noreply.github.com>
2024-12-06 09:39:48 +0000

    deploy: 57d96b940ca71e2a48c152ef51164de75ec84d0a

--- toc.js
+++ toc.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item "><a href="index.html"><strong aria-hidden="true">1.</strong> Table of Contents</a></li><li class="chapter-item "><a href="mathematics/entropy.html"><strong aria-hidden="true">2.</strong> Probability distribution and Entropy</a></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item "><a href="index.html"><strong aria-hidden="true">1.</strong> Table of Contents</a></li><li class="chapter-item "><a href="mathematics/entropy.html"><strong aria-hidden="true">2.</strong> Probability distribution and Entropy</a></li><li class="chapter-item "><a href="mathematics/k-means-with-cos-distance.html"><strong aria-hidden="true">3.</strong> k-means 聚类中使用余弦距离 cos distance</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString();
         if (current_page.endsWith("/")) {
